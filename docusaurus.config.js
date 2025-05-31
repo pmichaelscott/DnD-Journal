bfff2dc4627c78beef416dbf2b87d1d8185cbaf4
Paul Scott <pmichaelscott@gmail.com>
2025-05-31 13:39:31 -0700

    Removed the blog section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,8 +52,8 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -63,8 +63,8 @@ const config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -82,7 +82,7 @@ const config = {
     ({
       // Replace with your project's social card
       // image: 'img/docusaurus-social-card.jpg',
-      metadata: [{name: 'og:description', content: 'A place on the internet'}]
+      metadata: [{name: 'og:description', content: 'A place on the internet'}],
       navbar: {
         title: 'Peezie\'s Place',
         // logo: {
